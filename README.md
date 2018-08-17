55304b195bcd281d1d3de1d32242059d74230478
Robert Kieffer <robert@broofa.com>
2018-08-16 17:32:42 -0700

    chore: readme style

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
 
 # BroofaJS
 
-Misc. JS utilities that may be of broader interest.  See subdirectories for documentation
+Misc. JS utilities that may be of broader interest
 
-
-## [@broofa/asyncproxy](asyncproxy)
-
-A simple, intuitive solution for converting node-style APIs to Promises.
-    
-
-## [@broofa/stringlang](stringlang)
-
-Count string characters by unicode block
-    
-    
+| module | description |
+| --- | --- |
+| [@broofa/asyncproxy](asyncproxy) | A simple, intuitive solution for converting node-style APIs to Promises. |
+| [@broofa/stringlang](stringlang) | Count string characters by unicode block |
--- build/readme.js
+++ build/readme.js
@@ -7,25 +7,23 @@ const path = require('path');
 async function main() {
   const nodes = fs.readdirSync('.');
   let subpackages = await globby(['**/*/package.json', '!**/node_modules/**']);
-  console.log(subpackages);
   subpackages = subpackages.map(path => {
     const pkg = require('../' + path);
     const pkgName = path.replace('/package.json', '');
+    console.log(pkg.name, pkg.description);
 
-    return `
-## [${pkg.name}](${pkgName})
-
-${pkg.description}
-    `
+    return `| [${pkg.name}](${pkgName}) | ${pkg.description} |`
   });
 
   fs.writeFileSync(path.join(__dirname, '../README.md'), `
 # BroofaJS
 
-Misc. JS utilities that may be of broader interest.  See subdirectories for documentation
+Misc. JS utilities that may be of broader interest
 
+| module | description |
+| --- | --- |
 ${subpackages.join('\n')}
-    `);
+`);
 }
 
 main();
