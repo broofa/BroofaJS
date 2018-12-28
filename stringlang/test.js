c1edcbae7a35ffa19a1be9692bb81586bd65b91c
Robert Kieffer <robert@broofa.com>
2018-12-28 05:53:59 -0800

    fixup test

--- stringlang/test.js
+++ stringlang/test.js
@@ -1,4 +1,14 @@
+const assert = require('assert');
 const StringLang = require('.');
 
-const sl = new StringLang('Hello World or Καλημέρα κόσμε or こんにちは 世界');
-console.log(sl);
+assert.deepEqual(
+  new StringLang('Hello World or Καλημέρα κόσμε or こんにちは 世界').inspect(),
+  {
+    basicLatin: 21,
+    cjkUnifiedIdeographs: 2,
+    greekandCoptic: 13,
+    hiragana: 5
+  }
+);
+
+console.log('All tests passed');
